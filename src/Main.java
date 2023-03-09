dfc610d4e834dc65e67d394dbfced97cef68c724
Anton <toniq133@bk.ru>
2023-03-09 14:24:31 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 100;
+        int am = 1200;
+        if (am < 1000) {
+            a = a + am;
+        }
+        if (am > 999) {
+            int bon = am / 100;
+            a = a + am + bon;
+        }
+        System.out.println(a);
     }
 }
