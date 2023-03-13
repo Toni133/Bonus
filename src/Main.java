0df7fa1382e3e23040ed59c79119f98bf55fcd16
Anton <toniq133@bk.ru>
2023-03-13 17:06:22 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int refill = 1000;
-        if (refill < 1001) {
+        int refill = 1001;
+        if (refill <= 1000) {
             account = account + refill;
         }
-        if (refill > 1001) {
+        if (refill >= 1001) {
             int bonus = refill / 100;
             account = account + refill + bonus;
         }
